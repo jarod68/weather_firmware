475c37357dac488f4695bc8337e8f52df4196732
milou68@gmail.com <milou68@gmail.com>
2014-03-15 19:58:53 +0100

    Add header comment

--- weather_station/AClock.h
+++ weather_station/AClock.h
@@ -1,3 +1,12 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** Author: Matthieu Holtz
+** Year: 2014
+** -------------------------------------------------------------------------*/
+
 #pragma once
 #ifndef ACLOCK_H
 #define ACLOCK_H
--- weather_station/NTPClock.h
+++ weather_station/NTPClock.h
@@ -1,3 +1,11 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** Author: Matthieu Holtz
+** Year: 2014
+** -------------------------------------------------------------------------*/
 #pragma once
 
 #ifndef NTPCLOCK_H
